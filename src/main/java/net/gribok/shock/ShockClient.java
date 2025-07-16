eafe1e90e85ba19ee496741b33273c27aa474b38
NikitaBenia <volmik07@gmail.com>
2025-07-16 17:27:06 +0300

    Initial commit

---
+++ src/main/java/net/gribok/shock/ShockClient.java
@@ -0,0 +1,10 @@
+package net.gribok.shock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShockClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/gribok/shock/ShockDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gribok.shock;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ShockDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
